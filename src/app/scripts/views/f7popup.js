8a9a339cc4afcfacfc5800792f6f5ddfd34e5060
Luiz Américo <brandkarl@gmail.com>
2017-01-07 12:24:30 -0300

    Optimize and simplify F7Popup

--- src/app/scripts/views/f7popup.js
+++ src/app/scripts/views/f7popup.js
@@ -1,27 +1,23 @@
 var Marionette = require('marionette');
 
 module.exports = Marionette.Behavior.extend({
-  initialize: function (options) {
-    this.id = this.view.options.id || options.id
-  },
 
   events: {
     'click .close': 'onCloseClick',
-    'click .finish': 'onCloseClick'
+    'click .finish': 'onCloseClick',
+    'closed': 'onClosed'
   },
 
   onCloseClick: function(e) {
-    window.f7.closeModal('#' + this.id);
+    window.f7.closeModal(this.view.el);
 
     e.preventDefault();
     return false;
   },
 
   onRender: function() {
-    var popupEl = window.f7.popup(this.view.template());
-    this.view.setElement(popupEl); // Reload events
-
-    $('#' + this.id).on('closed', _.bind(this.onClosed, this));
+    this.$el.addClass('popup');
+    window.f7.popup(this.view.el);
   },
 
   onClosed: function() {
